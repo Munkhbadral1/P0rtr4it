0060bd45cc27cd1841f1b347ea8b044b8d11c865
Munkhbadral1 <rizyl75@gmail.com>
2023-01-28 23:34:19 +0800

    1

--- Student of MUST-SICT/cf.cpp
+++ Student of MUST-SICT/cf.cpp
@@ -7,12 +7,12 @@ int main(){
     int counter1=0;
     myFile.open("2022.11.30-4.txt", ios::out);
     if(myFile.is_open()){
-        https://student.must.edu.mn/Student/GetImage?u021EE=B210970318
+        https://student.must.edu.mn/Student/GetImage?u021EE=B
         for(int a=1; a=2; a++){
             for(int b=0; b<10; b++){
                 for(int c=0; c<10; c++){
                     for(int d=0; d<10; d++){
-                        myFile<<"<img width=\"60px\" src=\"https://student.must.edu.mn/Student/GetImage?u021EE=B"<<a<<"10970318"<<a<<b<<c<<d<<".jpg\">"<<"\n";
+                        myFile<<"<img width=\"60px\" src=\"https://student.must.edu.mn/Student/GetImage?u021EE=B"<<a<<"1"<<a<<b<<c<<d<<".jpg\">"<<"\n";
                     }           
                 }
             }
